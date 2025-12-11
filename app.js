806d4c5892a2f1a0a3a6afe2374d938264748b45
MisterConsistent <bladeandsoulsucks@gmail.com>
2025-12-11 18:36:16 +0200

    Add backend tests and config improvements

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ const morgan = require("morgan");
 const cors = require("cors");
 const errorHandler = require("./middleware/errorHandler");
 
-// ******************************************************
-// Αφαιρέθηκε η γραμμή require("dotenv").config();
-// για να μην φορτώνεται κατά τις δοκιμές.
-// Θα πρέπει να φορτωθεί στο server.js ή στο index.js!
-// ******************************************************
+// ... (άλλα require)
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+// ----------------------------------------------------
+// !!! ΠΡΟΣΘΗΚΗ ΤΟΥ ΑΡΧΙΚΟΥ ROUTE ΓΙΑ ΤΟ ΒΑΣΙΚΟ PATH !!!
+// ----------------------------------------------------
+app.get("/", (req, res) => {
+  res.send("Emergensh!t API is running");
+});
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/toilets", require("./routes/toilets"));
